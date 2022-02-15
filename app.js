a2ee0066674db3cf9c7305317d742a329e69ce2b
thermalthrottle <biz.mitko@gmail.com>
2022-02-15 20:27:31 +0200

    Base app

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const env = dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    const message = "test123";
+    res.status(200).json({message});
+});
+
+const port = process.env.PORT || 3200
+
+app.listen(port, () => {
+    console.log(`Microservice is up at port ${port}`);
+});
