852f3db38cda1a893eb88fa8bddf68b6b5de6a23
thermalthrottle <biz.mitko@gmail.com>
2022-02-16 21:14:10 +0200

    Base service methods

---
+++ src/services/RestaurantService.js
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+
+import Restaurants from '../models/Restaurants.js';
+
+class RestaurantsService {
+
+    constructor(){}
+
+    async CreateRestaurant(name, pictures, contacts, locationLink) {
+
+
+
+    }
+
+    async AddMenuItems(name, description, price, pictures) {
+
+
+
+    }
+
+    async FindRestaurant(name, description, price, pictures) {
+
+
+
+    }
+
+    async FindMenuItem() {
+
+
+
+    }
+
+    async UpdateRestaurantInfo(name, pictures, contacts, locationLink) {
+        
+
+
+    }
+
+    async UpdateMenuItems() {
+
+
+
+    }
+
+    async DeleteRestaurant() {
+
+
+
+    }
+
+    async DeleteMenuItems() {
+
+
+
+    }
+
+}
