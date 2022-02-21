7aa1243092dc8d169a689adac36b7b733de4a641
thermalthrottle <biz.mitko@gmail.com>
2022-02-21 16:50:40 +0200

    Added Keycloak initialization

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,18 @@
 import express from "express";
 
 import RestaurantController from "./controllers/RestaurantsController.js";
+import KeycloakInit from "./helpers/InitKeycloak.js";
 
 const router = express.Router();
 
 const restaurantController = new RestaurantController();
 
-router.get('/', (req, res) => {
-    const message = "test123";
-    res.status(200).json({message});
-});
+const keycloak = KeycloakInit();
 
-router.post('/create', (req, res) => restaurantController.CreateRestaurant(req, res));
-router.get('/find', (req, res) => restaurantController.FindRestaurant(req, res));
+router.get('/find', async (req, res) => restaurantController.FindRestaurant(req, res));
+
+router.use(keycloak.middleware());
+
+router.post('/create', keycloak.protect(), async (req, res) => restaurantController.CreateRestaurant(req, res));
 
 export default router;
